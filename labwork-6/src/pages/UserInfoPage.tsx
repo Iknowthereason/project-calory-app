06286af40b767bb9aced28d99955cf6857937fd7
Markus Kitunen <markus.kitunen@outlook.com>
2023-11-28 19:50:35 +0200

    updated

--- labwork-6/src/pages/UserInfoPage.tsx
+++ labwork-6/src/pages/UserInfoPage.tsx
@@ -1,33 +1,21 @@
 import { FunctionComponent, useState, useCallback, MouseEvent } from "react";
 import { IonButton, IonCard, IonCardContent, IonContent, IonGrid, IonHeader, IonInput, IonCol, IonList, IonPage, IonRow, IonTitle, IonToolbar, IonText, IonRadio, IonRadioGroup, IonLabel, IonSegment, IonSegmentButton, IonButtons, IonMenuButton } from "@ionic/react";
-const UserInfoPage: FunctionComponent = () => {
-  const [userInfo, setUserInfo] = useState<IUserInfoState>({ age: "", height: "", weight: "", activityLevel: "", medicalProblems: false })
-  const [units, setUnits] = useState<string>("metric")
+import { IUserProps, IUserInfoState } from '../interfaces/interfaces'
+const UserInfoPage: React.FC<IUserProps> = ({ user, setUser }) => {
   
-
-  interface IUserInfoState {
-    age: String
-    height: String
-    weight: String
-    activityLevel: "low" | "medium" | "high" | ""
-    medicalProblems: Boolean
-
-  }
-    
-  const handleUnitChange = (event: MouseEvent<HTMLIonSegmentButtonElement, globalThis.MouseEvent>) => {
-    setUnits((event.target as HTMLInputElement).value)
-  }
+  const [userInfo, setUserInfo] = useState<IUserInfoState>({ age: "", height: "", weight:"", activityLevel: "", medicalProblems: false })
 
   const handleChange = (event: any) => {
-    if (units === "imperial") {
-
-    }
     setUserInfo({ ...userInfo, [event.target.name]: event.target.value })
-    console.log(event)
   }
 
   const handleActivityLevelChange = (event: any) => {
-    setUserInfo({ ...userInfo, activityLevel: event.target.value})
+    setUserInfo({ ...userInfo, activityLevel: event.target.value })
+  }
+
+  const handleSave = () => {
+    setUser(userInfo)
+    
   }
   
   return (
@@ -43,24 +31,8 @@ const UserInfoPage: FunctionComponent = () => {
         </IonHeader>
         <IonContent>
           <IonLabel>
-            <h1 style={{ margin: "10px" }}>Please fill in the user information</h1></IonLabel>
-   {/*        <IonText>
-            <h4>Please fill in the user information</h4>
-          </IonText> */}
-          <IonGrid>
-            <IonRow class="ion-justify-content-center">
-              <IonCol size="6">
-                <IonSegment value={units}>
-                  <IonSegmentButton className="units-toggle" value="metric" onClick={handleUnitChange}>
-                    <IonLabel>Metric</IonLabel>
-                  </IonSegmentButton>
-                  <IonSegmentButton className="units-toggle" value="imperial" onClick={handleUnitChange}>
-                    <IonLabel>Imperial</IonLabel>
-                  </IonSegmentButton>
-                </IonSegment>
-              </IonCol>
-            </IonRow>
-          </IonGrid>
+            <h1 style={{ margin: "10px" }}>Please fill in the user information</h1>
+          </IonLabel>
           <IonCard>
             <IonCardContent className="bodyContainer">
               <IonGrid>
@@ -79,8 +51,6 @@ const UserInfoPage: FunctionComponent = () => {
                     ></IonInput>
                   </IonCol>
                 </IonRow>
-                {units === "metric" ?
-                <div>
                   <IonRow>
                     <IonCol color="background-color">
                       <IonLabel>Height (cm)</IonLabel>
@@ -111,58 +81,6 @@ const UserInfoPage: FunctionComponent = () => {
                       </IonInput>
                     </IonCol >
                   </IonRow>
-                  </div>
-                  :
-                  <div>
-                  <IonRow>
-                    <IonRow>
-                      <IonLabel>&nbsp;Height</IonLabel>
-                    </IonRow>
-                    <IonRow>
-                      <IonCol color="background-color">
-                        <IonInput
-                          type="number"
-                          className="input" 
-                          labelPlacement="stacked" 
-                          fill="outline" 
-                          label="Feet" 
-                          placeholder="Enter feet"
-                          value={userInfo.height.toString()}
-                          onIonChange={handleChange}
-                          name="height"></IonInput>
-                      </IonCol>
-                      <IonCol color="background-color">
-                        <IonInput
-                        type="number"
-                        className="input" 
-                        labelPlacement="stacked" 
-                        fill="outline" 
-                        label="Inches" 
-                        placeholder="Enter inches"
-                        value={userInfo.height.toString()}
-                        onIonChange={handleChange}
-                        name="height"></IonInput>
-                      </IonCol>
-                    </IonRow>
-                  </IonRow>
-                  <IonRow>
-                    <IonCol color="background-color">
-                      <IonLabel>Weight (lb)</IonLabel>
-                        <IonInput
-                          type="number" 
-                          className="input" 
-                          labelPlacement="stacked" 
-                          fill="outline"  
-                          placeholder="Enter Weight"
-                          value={userInfo.weight.toString()}
-                          onIonChange={handleChange}
-                          name="weight">
-                      </IonInput>
-                    </IonCol >
-                  </IonRow>
-                </div>
-                }
-               
                 <IonRow>
                   <IonCol>
                     <IonText>
@@ -205,7 +123,7 @@ const UserInfoPage: FunctionComponent = () => {
           <IonGrid>
             <IonRow>
               <IonCol>
-                <IonButton expand="block">Save</IonButton>
+                <IonButton expand="block" onClick={handleSave}>Save</IonButton>
               </IonCol>
             </IonRow>
           </IonGrid>
